76a3d6155ea853dc92ec0d29d6f445a840b6b000
Yushin Ito <yitengyouxin8@gmail.com>
2022-08-13 17:03:14 +0900

    Remove: sampleを削除

--- sample/stream.js
+++
@@ -1,9 +0,0 @@
-import { ChatListener } from "mildom-api";
-
-const onChat = async (chat) => {
-  console.log(chat.msg);
-};
-
-const listener = new ChatListener(13650940);
-listener.on("onChat", onChat);
-await listener.startListener();
